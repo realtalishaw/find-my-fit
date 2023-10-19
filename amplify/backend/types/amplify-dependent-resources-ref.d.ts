d60960a26f6468c7ab7bf6cfde6f84c23876d220
Talisha White <talishawhite@Talishas-MacBook-Pro.local>
2023-10-19 19:21:23 -0400

    api stuff

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,23 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
+    "api9be5c04d": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    },
     "findmyfit": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "function": {
+    "findmyfitedb747e5": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
--- src/Components/MultiStepForm.js
+++ src/Components/MultiStepForm.js
@@ -6,6 +6,7 @@ import AdditionalQuestions from './AdditionalQuestions';
 import EmailCapture from './EmailCapture';
 import Results from './Results';
 import HomeScreen from './HomeScreen';
+import { API } from 'aws-amplify';
 
 function MultiStepForm({ title, subtitle }) {
   const { brand } = useParams();
@@ -16,33 +17,32 @@ function MultiStepForm({ title, subtitle }) {
   const displayTitle = brand ? `${brand} Size Guide` : title;
   const displaySubtitle = brand ? "Get your perfect size in seconds" : subtitle;
 
-  const onSubmit = (data, event) => {
-    console.log("Form data submitted:", data);
-    if (event) event.preventDefault();
-  
-    if (currentStep === 4) {
-           // Call your API with the collected data
-      console.log(data);
-      // Advance to the Results step
-      setCurrentStep(currentStep + 1)
-      
-    } else {
-        console.log("Current Step is..", currentStep)
-        setCurrentStep(currentStep + 1);
-    }
-  };
-  
-  
+
   
 
-  const handleNextStep = (fitPreference) => {
+  const handleNextStep = async (fitPreference, data) => {
     setValue('additionalQuestions.fitPreference', fitPreference);  // Update form data
-    setCurrentStep(currentStep + 1);  // Proceed to the next step
+    if (currentStep === 4) {
+        console.log("Step 4")
+        try {
+          const result = await API.post('api9be5c04d', '/size-recommendations', {
+            body: data
+          });
+          console.log(result);
+          // Advance to the Results step
+          setCurrentStep(currentStep + 1);
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        console.log("Current Step is..", currentStep)
+        setCurrentStep(currentStep + 1);
+      }
   };
 
   return (
-    <FormProvider {...methods}>  {/* Spread form methods into FormProvider */}
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(() => handleNextStep())}>
         {currentStep === 1 && <HomeScreen title={displayTitle} subtitle={displaySubtitle} onNext={() => setCurrentStep(currentStep + 1)} />}
         {currentStep === 2 && (
           <Controller
@@ -62,17 +62,14 @@ function MultiStepForm({ title, subtitle }) {
             )}
           />
         )}
-{currentStep === 4 && (
-      <Controller
-        name="emailCapture"
-        control={control}
-        render={({ field }) => <EmailCapture {...field} onGetResults={() => setCurrentStep(5)} />}
-      />
-    )}
-
+        {currentStep === 4 && (
+          <Controller
+            name="emailCapture"
+            control={control}
+            render={({ field }) => <EmailCapture {...field} onGetResults={() => handleNextStep()} />}
+          />
+        )}
         {currentStep === 5 && <Results data={getValues()} />}
-        {currentStep < 5 && <button type="submit">Next</button>}
-        {currentStep > 1 && <button type="button" onClick={() => setCurrentStep(currentStep - 1)}>Back</button>}
       </form>
     </FormProvider>
   );
